94cf0e3fec5334c6b525335f9487e7724b5a4f91
Joao <jmmsp2003@hotmail.com>
2024-04-26 23:58:27 -0300

    erro no atributo minas resolvido

--- Cod3r/campo_minado/src/br/com/cod3r/cm/modelo/Campo.java
+++ Cod3r/campo_minado/src/br/com/cod3r/cm/modelo/Campo.java
@@ -78,6 +78,10 @@ public class Campo {
 			minado = true;	
 	}
 	
+	public boolean isMinado() {
+		return minado;
+	}
+	
 	public boolean isMarcado() {
 		return marcado;
 	}
--- Cod3r/campo_minado/src/br/com/cod3r/cm/modelo/Tabuleiro.java
+++ Cod3r/campo_minado/src/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -2,6 +2,7 @@ package br.com.cod3r.cm.modelo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Tabuleiro {
 
@@ -22,7 +23,7 @@ public class Tabuleiro {
 	}
 
 	// i = linha ;; j = colunas
-	private void gerarCampos() {
+	public void gerarCampos() {
 		for(int linha = 0; linha < linhas; linha++) {
 			for(int coluna = 0; coluna < colunas; coluna++) {
 				campos.add(new Campo(linha, coluna));
@@ -30,7 +31,7 @@ public class Tabuleiro {
 		}
 	}
 	
-	private void assossiarOsVizinhos() {
+	public void assossiarOsVizinhos() {
 		for(Campo c1: campos) {
 			for(Campo c2: campos ) {
 				c1.adicionarVizinho(c2);
@@ -38,10 +39,47 @@ public class Tabuleiro {
 		}
 	}
 	
-	private void sortearMinas() {
+	public void sortearMinas() {
+		long minasArmadas = 0;
+		Predicate<Campo> minado = c -> c.isMinado();
+				
+		do {
+			minasArmadas = campos.stream().filter(minado).count();
+			int aleatorio = (int) (Math.random() * campos.size());
+			campos.get(aleatorio).minar();
+			
+		}while (minasArmadas < minas);
 		
 	}
+	
+	public boolean objetivoAlcancado() {
+		return campos.stream().allMatch(c -> c.objetivoAlcancado()); 
+	}
 
+	public void reinicia() {
+		campos.stream().forEach(c -> c.reiniciarJogo());
+		sortearMinas();
+	}
+	
 
+    @Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		
+		int i = 0;
+		for(int l = 0; l < linhas; l++) {
+			
+			for(int c = 0; c < colunas ; c++) {
+				sb.append(" ");
+				sb.append(campos.get(i));
+				sb.append(" ");
+				i++;
+			}
+			sb.append("\n");
+		}
+		
+		return sb.toString();
+	}
 	
 }
+
